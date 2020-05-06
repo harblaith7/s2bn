b8af9dd8100d48f6932a78dcba3baf2065457e92
Laith Harb <harblaith77@gmail.com>
2020-05-06 13:26:42 -0400

    Completed header and nav design

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -1,11 +1,44 @@
 import React, { Component } from 'react'
 import "./Nav.scss"
+import s2bn from "../../assets/images/svg/s2bn.svg"
 
 export default class Nav extends Component {
     render() {
         return (
             <nav className = "Nav">
-                
+               <ul className="Nav__list">
+                   <li className="Nav__list-item">
+                       <a href="" className="Nav__link">
+                            <img src={s2bn} alt="" className="Nav__logo"/>
+                       </a>
+                   </li>
+                   <li className="Nav__list-item Nav__list-item--right">
+                       <a href="" className="Nav__link">
+                            About
+                       </a>
+                   </li>
+                   <li className="Nav__list-item">
+                       <a href="" className="Nav__link">
+                            Chapters
+                       </a>
+                   </li>
+                   <li className="Nav__list-item">
+                       <a href="" className="Nav__link">
+                            Events
+                       </a>
+                   </li>
+                   
+                   <li className="Nav__list-item">
+                       <a href="" className="Nav__link">
+                            Sponsorship
+                       </a>
+                   </li>
+                   <li className="Nav__list-item">
+                       <a href="" className="Nav__link">
+                            Contact
+                       </a>
+                   </li>
+               </ul>
             </nav>
         )
     }
--- client/src/pages/LandingPage/LandingPage.tsx
+++ client/src/pages/LandingPage/LandingPage.tsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import Header from "../../components/Header/Header"
 import Nav from "../../components/Nav/Nav"
+import "./LandingPage.scss"
 
  class LandingPage extends Component {
     render() {
         return (
-            <>
+            <div className="LandingPage">
                 <Nav/>
                <Header/> 
-            </>
+            </div>
         )
     }
 }
