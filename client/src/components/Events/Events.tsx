7a2f80da8f43d2f1d267ae7cf80e72bc4a008345
Laith Harb <harblaith77@gmail.com>
2020-05-12 12:40:38 -0400

    added bootstrap slider to events section

--- client/src/components/Events/Events.tsx
+++ client/src/components/Events/Events.tsx
@@ -1,21 +1,27 @@
 import React, { Component } from 'react'
 import "./Events.scss"
 import EventCard from "../EventCard/EventCard"
+import Carousel from "react-bootstrap/Carousel"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class Events extends Component {
     render() {
         return (
             <div className = "Events">
                 <div className="Events__container">
-                    <h2 className="Events__header">
-                        Upcoming Events
-                    </h2>
-                    <div className="Events__line"></div>
-                    <div className="Events__event-card-container">
-                        <EventCard />
-                        <EventCard />
-                        <EventCard />
-                    </div>
+                    <Carousel>
+                        <Carousel.Item>
+                            <EventCard/>
+                        </Carousel.Item>
+
+                        <Carousel.Item>
+                            <EventCard/>
+                        </Carousel.Item>
+                        
+                        <Carousel.Item>
+                            <EventCard/>
+                        </Carousel.Item>
+                    </Carousel>
                 </div>
             </div>
         )
