cd34f3b5d283ef50feb4f929a95ea1a31b8fd743
Laith Harb <harblaith77@gmail.com>
2020-06-30 19:56:17 -0400

    Wrote test to ensure page loads properly

---
+++ tests/RouteTests/load.test.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer')
+
+let browser, page;
+jest.setTimeout(300000)
+
+beforeEach(async () => {
+    browser = await puppeteer.launch({
+        headless: false
+    })
+    page = await browser.newPage()
+    page.goto("http://localhost:3000/")
+})
+
+afterEach(async () => {
+    await browser.close()
+})
+
+
+test("Appropriate text appears on landing page", async () => {
+    const html = await page.$eval(".Header__text", e => e.innerHTML)
+    expect(html).toEqual("We help students gain a competitive edge in both school and business")
+})
+
