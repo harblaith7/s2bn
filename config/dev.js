a4c49056a876c9f0270245457cb0e96776d79b42
Laith Harb <harblaith77@gmail.com>
2020-07-04 09:37:30 -0400

    changes

--- config/dev.js
+++ config/dev.js
@@ -1,10 +1,14 @@
 const MongoURI = `mongodb+srv://s2bn:xN2Dvf4hHx1lq8gK@cluster0-b2yl2.mongodb.net/<dbname>?retryWrites=true&w=majority`;
 const JWTSecret = "sadasfajoqjio34t328tgn3oglsdg3u4ojsdfj2o";
-const authCode = "faslf34j231l!@!#vlnkfds39kds?f"
+const authCode = "faslf34j231l!@!#vlnkfds39kds?f";
+const email = "laithharden@hotmail.com";
+const password = "loliloveyou"
 
 
 module.exports = {
     MongoURI,
     JWTSecret,
-    authCode
+    authCode,
+    email,
+    password
 }
