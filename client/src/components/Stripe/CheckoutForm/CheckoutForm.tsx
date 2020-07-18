0a7f8ccb2aa39ba8824559559be6ba0d5861da2c
Laith Harb <harblaith77@gmail.com>
2020-07-17 22:42:44 -0400

    Created payment modal UI

--- client/src/components/Stripe/CheckoutForm/CheckoutForm.tsx
+++ client/src/components/Stripe/CheckoutForm/CheckoutForm.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./CheckoutForm.scss"
+import {CardElement} from "@stripe/react-stripe-js"
 
 const CheckoutForm = () => {
     return (
@@ -9,25 +10,37 @@ const CheckoutForm = () => {
                 placeholder="Full Name" 
                 className="CheckoutForm__input"
                 name="fullName"
+                required
             />
             <input 
                 type="text" 
                 placeholder="Email" 
                 className="CheckoutForm__input"
                 name="email"
+                required
             />
             <input 
                 type="text" 
                 placeholder="Address" 
                 className="CheckoutForm__input"
                 name="email"
+                required
             />
             <input 
                 type="text" 
                 placeholder="Phone Number" 
                 className="CheckoutForm__input"
                 name="number"
+                required
             />
+            <CardElement
+                options={{
+                    hidePostalCode: true
+                }}
+            />
+            <button type="submit" className="CheckoutForm__btn">
+                Pay
+            </button>
         </form>
     )
 }
