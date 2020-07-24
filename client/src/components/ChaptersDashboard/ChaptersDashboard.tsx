d5fc05a78f84d8bb677e03d421c347655f2955b0
Laith Harb <harblaith77@gmail.com>
2020-07-23 21:26:29 -0400

    Created of UI change snippet

---
+++ client/src/components/ChaptersDashboard/ChaptersDashboard.tsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import "./ChaptersDashboard.scss"
+
+interface IState {
+    cities: String[],
+    activeCity: String
+}
+
+export default class ChaptersDashboard extends Component<{}, IState> {
+
+    constructor(props: {}){
+        super(props)
+        this.state = {
+            cities: ["London", "Waterloo", "Ottawa", "Toronto", "Edmonton", "Guelph", "Kingston"],
+            activeCity: "London"
+        }
+    }
+
+    displayTabs = () => {
+        return this.state.cities.map(city => {
+            return (
+                <li 
+                    className={`ChaptersDashboard__tab ${this.state.activeCity === city && "ChaptersDashboard__tab--active"}`}
+                    onClick={this.changeCity}
+                    id={`${city}`}
+                >
+                    {city}
+                </li>
+            )
+        })
+    }
+
+    changeCity = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+        this.setState({
+            activeCity: e.target.id
+        })
+    }
+
+    render() {
+        return (
+            <div className="ChaptersDashboard">
+                <div className="ChaptersDashboard__container">
+                    <h4 className="ChaptersDashboard__title">
+                        Chapters
+                    </h4>
+                    <nav className="ChaptersDashboard__nav">
+                        {this.displayTabs()}
+                    </nav>
+                    <div className="ChaptersDashboard__fields-container">
+                        <form action="" className="ChaptersDashboard__form">
+                            <p className="ChaptersDashboard__text">
+                                Chapters Snippet
+                            </p>
+                            <textarea className="ChaptersDashboard__textarea"></textarea>
+                            <input className="ChaptersDashboard__submit" type="submit" value="Update Chapter Snippet"/>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
--- client/src/pages/DashboardPage/DashboardPage.tsx
+++ client/src/pages/DashboardPage/DashboardPage.tsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import "./DashboardPage.scss"
+import ChaptersDashboard from '../../components/ChaptersDashboard/ChaptersDashboard'
 
 class DashboardPage extends Component {
     render() {
         return (
             <div className="DashboardPage">
-
+                <ChaptersDashboard/>
             </div>
         )
     }
